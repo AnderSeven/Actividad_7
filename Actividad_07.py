f4b46a073f48b25cca89984e275992a9ba92d350
AnderSeven <andersonmanoloalvaradoalvarez@gmail.com>
2025-07-16 18:45:40 -0600

    creacion del menu

---
+++ Actividad_07.py
@@ -0,0 +1,9 @@
+estudiantes = {}
+opciones = 0
+p = False
+while p == False:
+    print("====Menu====")
+    print("1. Registrar estudiantes")
+    print("2. Mostrar todos los estudiantes y sus cursos")
+    print("3. Buscar estudiante por carnet")
+    print("4. Salir")
